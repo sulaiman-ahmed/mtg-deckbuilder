ef4bb77c75fc8280ecf1b37dd93008c1183c06b7
sulaiman-ahmed <sulaimanahmed08@gmail.com>
2024-05-15 17:18:40 -0400

    added export feature to decklist

--- src/components/DeckList.tsx
+++ src/components/DeckList.tsx
@@ -3,9 +3,27 @@ import { DeckListProps } from "./types";
 import { Box, Button, Grid, Typography } from "@mui/material";
 
 const DeckList: React.FC<DeckListProps> = ({ cards, onRemoveCard }) => {
+
+    const exportDeck = () => {
+        const deckListText = cards.map(card => card.name).join('\n');
+        const deckBlob = new Blob([deckListText], { type: 'text/plain' });
+        const url = URL.createObjectURL(deckBlob);
+        const a = document.createElement('a');
+        a.href = url;
+        a.download = 'decklist.txt';
+        document.body.appendChild(a);
+        a.click();
+        document.body.removeChild(a);
+        URL.revokeObjectURL(url);
+    }
+
     return (
         <Box m={2}>
-            <Typography variant="h5">Current Decklist</Typography>
+            <Typography variant="h5">
+                Current Decklist
+                <Button onClick={exportDeck} variant="outlined" color="primary" style={{ marginLeft: '10px' }}>
+                    Export
+                </Button></Typography>
             <Typography variant="subtitle1">Total cards: {cards.length}</Typography>
             <Grid container spacing={2}>
                 {cards.map((card, index) => (
